d72f737c0ec8831e3c3830b04e8b8899301c17d0
Arham Arshad <68512793+arhamarshad1@users.noreply.github.com>
2022-07-05 20:06:34 -0300

    Updated Tables

--- CompanyY.sql
+++ CompanyY.sql
@@ -1,6 +1,7 @@
+-- -----------------------------------------------------
+-- Tables for Company Y
+-- -----------------------------------------------------
 USE aarshad;
--- MySQL Workbench Forward Engineering
-
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
@@ -10,9 +11,9 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Table `Client939`
+-- Table `CompY_Client939`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Client939` (
+CREATE TABLE IF NOT EXISTS `CompY_Client939` (
   `clientID939` INT NOT NULL AUTO_INCREMENT,
   `clientName939` VARCHAR(45) NOT NULL,
   `clientCity939` VARCHAR(45) NULL,
@@ -25,28 +26,28 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `PO's939`
+-- Table `CompY_POs939`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `PO's939` (
+CREATE TABLE IF NOT EXISTS `CompY_POs939` (
   `poNumber939` INT NOT NULL AUTO_INCREMENT,
   `datePO939` DATE NOT NULL,
   `status939` VARCHAR(45) NULL,
-  `Client939_clientID939` INT NOT NULL,
   `quantity939` INT NOT NULL,
+  `CompY_Client939_clientID939` INT NOT NULL,
   PRIMARY KEY (`poNumber939`),
-  INDEX `fk_PO's939_Client939_idx` (`Client939_clientID939` ASC),
-  CONSTRAINT `fk_PO's939_Client939`
-    FOREIGN KEY (`Client939_clientID939`)
-    REFERENCES `Client939` (`clientID939`)
+  INDEX `fk_CompY_POs939_CompY_Client9391_idx` (`CompY_Client939_clientID939` ASC),
+  CONSTRAINT `fk_CompY_POs939_CompY_Client9391`
+    FOREIGN KEY (`CompY_Client939_clientID939`)
+    REFERENCES `CompY_Client939` (`clientID939`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Parts939`
+-- Table `CompY_Parts939`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Parts939` (
+CREATE TABLE IF NOT EXISTS `CompY_Parts939` (
   `partNo939` INT NOT NULL AUTO_INCREMENT,
   `partName939` VARCHAR(45) NOT NULL,
   `partDescription939` VARCHAR(45) NULL,
@@ -58,26 +59,26 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Lines939`
+-- Table `CompY_Lines939`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Lines939` (
-  `PO's939_poNumber939` INT NOT NULL,
-  `Parts939_partNo939` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `CompY_Lines939` (
   `lineNo939` INT NOT NULL AUTO_INCREMENT,
   `qty939` INT NOT NULL,
   `priceOrdered939` INT NOT NULL,
-  PRIMARY KEY (`PO's939_poNumber939`, `Parts939_partNo939`),
-  INDEX `fk_PO's939_has_Parts939_Parts9391_idx` (`Parts939_partNo939` ASC),
-  INDEX `fk_PO's939_has_Parts939_PO's9391_idx` (`PO's939_poNumber939` ASC),
+  `CompY_POs939_poNumber939` INT NOT NULL,
+  `CompY_Parts939_partNo939` INT NOT NULL,
+  PRIMARY KEY (`CompY_POs939_poNumber939`, `CompY_Parts939_partNo939`),
   UNIQUE INDEX `lineNo939_UNIQUE` (`lineNo939` ASC),
-  CONSTRAINT `fk_PO's939_has_Parts939_PO's9391`
-    FOREIGN KEY (`PO's939_poNumber939`)
-    REFERENCES `PO's939` (`poNumber939`)
+  INDEX `fk_CompY_Lines939_CompY_POs9391_idx` (`CompY_POs939_poNumber939` ASC),
+  INDEX `fk_CompY_Lines939_CompY_Parts9391_idx` (`CompY_Parts939_partNo939` ASC),
+  CONSTRAINT `fk_CompY_Lines939_CompY_POs9391`
+    FOREIGN KEY (`CompY_POs939_poNumber939`)
+    REFERENCES `CompY_POs939` (`poNumber939`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_PO's939_has_Parts939_Parts9391`
-    FOREIGN KEY (`Parts939_partNo939`)
-    REFERENCES `Parts939` (`partNo939`)
+  CONSTRAINT `fk_CompY_Lines939_CompY_Parts9391`
+    FOREIGN KEY (`CompY_Parts939_partNo939`)
+    REFERENCES `CompY_Parts939` (`partNo939`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
