eb33b247db3a975eaef62b1b4a52519b9f82d9de
Arham Arshad <68512793+arhamarshad1@users.noreply.github.com>
2022-07-12 23:58:04 -0300

    insert lines into y

--- backend/routes/handler.js
+++ backend/routes/handler.js
@@ -90,6 +90,8 @@ router.post('/submitPO', async (req, res) => {
     let getPO = 0;
     let getPartNo = 0;
     let poNumberX = 0;
+    let getPartNoY = 0;
+    let poNumberY = 0;
 
     pool.getConnection( (err, conn) => {
         if (err) throw err;
@@ -112,23 +114,42 @@ router.post('/submitPO', async (req, res) => {
         });
             //handle lines for Company X and check if part exists
             JSON.parse(JSON.stringify(partName)).forEach(part => {
-            const partNumber = `SELECT partNo939 FROM CompX_Parts939 WHERE partName939 =`+"'"+part+"'";
-            conn.query(partNumber, (err, result2) => {
+            const partNumberX = `SELECT partNo939 FROM CompX_Parts939 WHERE partName939 =`+"'"+part+"'";
+            conn.query(partNumberX, (err, result2) => { //one
                 if (err) throw err;
                 getPartNo = JSON.stringify(result2);
-                JSON.parse(getPartNo).forEach(number => {
+                JSON.parse(getPartNo).forEach(number => { //two
                 const qryPOnumX = `SELECT poNumber939 FROM CompX_POs939 WHERE CompX_Client939_clientID939 = 7`;
-                conn.query(qryPOnumX, (err, result4) => {
+                conn.query(qryPOnumX, (err, result4) => { //three
                     if (err) throw err;
                     poNumberX = JSON.stringify(result4);
-                    JSON.parse(poNumberX).forEach(poX => {
+                    JSON.parse(poNumberX).forEach(poX => { //four
                         const checkPart = `INSERT INTO CompX_Lines939(qty939,CompX_POs939_poNumber939,CompX_Parts939_partNo939) VALUES(?, ?, ?)`;
-                        conn.query(checkPart, [quantity,poX.poNumber939,number.partNo939], (err, result3) => {
+                        conn.query(checkPart, [quantity,poX.poNumber939,number.partNo939], (err, result3) => { //five
                         if (err) throw err;
                 })
             })
         })     
-        //end
+        //end    
+        //insert into copany Y 
+        const partNumberY = `SELECT partNo939 FROM CompY_Parts939 WHERE partName939 =`+"'"+part+"'";
+        conn.query(partNumberY, (err, result2) => { //one
+            if (err) throw err;
+            getPartNoY = JSON.stringify(result2);
+            JSON.parse(getPartNo).forEach(number => { //two
+            const qryPOnumY = `SELECT poNumber939 FROM CompY_POs939 WHERE CompY_Client939_clientID939 = 7`;
+            conn.query(qryPOnumY, (err, result4) => { //three
+                if (err) throw err;
+                poNumberY = JSON.stringify(result4);
+                JSON.parse(poNumberY).forEach(poY => { //four
+                    const checkPartY = `INSERT INTO CompY_Lines939(qty939,CompY_POs939_poNumber939,CompY_Parts939_partNo939) VALUES(?, ?, ?)`;
+                    conn.query(checkPartY, [quantity,poY.poNumber939,number.partNo939], (err, result3) => { //five
+                    if (err) throw err;
+            }) 
+        }) 
+    }) 
+}) 
+})   
             const qry2 = `SELECT poNumber939 FROM CompZ_POs939 WHERE CompZ_Client939_clientID939 =`+clientID;
             conn.query(qry2, (err, result1) => {
             if (err) throw err;
