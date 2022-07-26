da42cca8354e726b0cc0ec73d9d18daa09df365a
Arham Arshad <68512793+arhamarshad1@users.noreply.github.com>
2022-07-26 01:18:52 -0300

    XA Transactions

--- backend/routes/handler.js
+++ backend/routes/handler.js
@@ -5,6 +5,7 @@ CSCI 4140 - Assignment 3
 
 Code adapted from: https://github.com/codrkai/node_react_mysql_db_tutorial
 */
+const { query } = require('express');
 const express = require('express');
 const router = express.Router();
 const pool =  require('../config/db.js');
@@ -98,6 +99,9 @@ router.get('/listPoWithLines', async (req, res) => {
 });
 
 router.post('/submitPO', async (req, res) => {
+    const transactionIDforX = 1;
+    const transactionIDforY = 2;
+    const transactionIDforZ = 3;
     const clientID = req.body.clientID;
     const quantity = req.body.reqQuantity;
     const partName = req.body.partName;
@@ -109,61 +113,96 @@ router.post('/submitPO', async (req, res) => {
 
     pool.getConnection( (err, conn) => {
         if (err) throw err;
-
-        const qry = `INSERT INTO CompZ_POs939 (datePO939, status939, CompZ_Client939_clientID939, quantity939) VALUES(NOW(),'Processing',?,?)`;
-        conn.query(qry, [clientID, quantity], (err, result) => {
+        //start all transactions
+        try{
+            conn.query("XA START " +"'"+transactionIDforZ+"'", (err, result) => {
+                if (err) throw err;
+                console.log('Transaction Z Started!');
+            });
+            const qry = `INSERT INTO CompZ_POs939 (datePO939, status939, CompZ_Client939_clientID939, quantity939) VALUES(NOW(),'Processing',?,?)`;
+            conn.query(qry, [clientID, quantity], (err, result) => {
             if (err) throw err;
-            console.log('Verififed!');
+            console.log('Transaction Query Successful for Z!');
+        });
+        //if query is successfull then END and PREPARE 
+        conn.query("XA END " +"'"+transactionIDforZ+"'", (err, result) => {
+            if (err) throw err;
+            console.log('Transaction Z Ended!');
+        });
+        conn.query("XA PREPARE " +"'"+transactionIDforZ+"'", (err, result) => {
+            if (err) throw err;
+            console.log('Transaction Z Prepared!');
+        });
+        //commit
+        conn.query("XA COMMIT " +"'"+transactionIDforZ+"'", (err, result) => {
+            if (err) throw err;
+            console.log('Transaction Z COMMITTED!');
+        });
+
+        conn.query("XA START " +"'"+transactionIDforX+"'", (err, result) => {
+            if (err) throw err;
+            console.log('Transaction X Started!');
         });
         const qryx = `INSERT INTO CompX_POs939 (datePO939, status939, CompX_Client939_clientID939, quantity939) VALUES(NOW(),'Processing',7,?)`;
         conn.query(qryx, [quantity], (err, result) => {
             if (err) throw err;
-            console.log('Verififed!');
+            console.log('Transaction Query Successful for X!');
+        });
+        conn.query("XA END " +"'"+transactionIDforX+"'", (err, result) => {
+            if (err) throw err;
+            console.log('Transaction X Ended!');
+        });
+        conn.query("XA PREPARE " +"'"+transactionIDforX+"'", (err, result) => {
+            if (err) throw err;
+            console.log('Transaction X Prepared!');
+        });
+        conn.query("XA COMMIT " +"'"+transactionIDforX+"'", (err, result) => {
+            if (err) throw err;
+            console.log('Transaction X COMMITTED!');
         });
 
+        conn.query("XA START " +"'"+transactionIDforY+"'", (err, result) => {
+            if (err) throw err;
+            console.log('Transaction Y Started!');
+        });
+    
         const qryY = `INSERT INTO CompY_POs939 (datePO939, status939, CompY_Client939_clientID939, quantity939) VALUES(NOW(),'Processing',4,?)`;
         conn.query(qryY, [quantity], (err, result) => {
             if (err) throw err;
-            console.log('Verififed!');
+            console.log('Transaction Query Successful for Y!');
         });
-            //handle lines for Company X and check if part exists
-            JSON.parse(JSON.stringify(partName)).forEach(part => {
-            const partNumberX = `SELECT partNo939 FROM CompX_Parts939 WHERE partName939 =`+"'"+part+"'";
-            conn.query(partNumberX, (err, result2) => { //one
-                if (err) throw err;
-                getPartNo = JSON.stringify(result2);
-                JSON.parse(getPartNo).forEach(number => { //two
-                const qryPOnumX = `SELECT poNumber939 FROM CompX_POs939 WHERE CompX_Client939_clientID939 = 7`;
-                conn.query(qryPOnumX, (err, result4) => { //three
-                    if (err) throw err;
-                    poNumberX = JSON.stringify(result4);
-                    JSON.parse(poNumberX).forEach(poX => { //four
-                        const checkPart = `INSERT INTO CompX_Lines939(qty939,CompX_POs939_poNumber939,CompX_Parts939_partNo939) VALUES(?, ?, ?)`;
-                        conn.query(checkPart, [quantity,poX.poNumber939,number.partNo939], (err, result3) => { //five
-                        if (err) throw err;
-                })
-            })
-        })     
-        //end    
-        //insert into copany Y 
-        const partNumberY = `SELECT partNo939 FROM CompY_Parts939 WHERE partName939 =`+"'"+part+"'";
-        conn.query(partNumberY, (err, result2) => { //one
+
+        conn.query("XA END " +"'"+transactionIDforY+"'", (err, result) => {
             if (err) throw err;
-            getPartNoY = JSON.stringify(result2);
-            JSON.parse(getPartNo).forEach(number => { //two
-            const qryPOnumY = `SELECT poNumber939 FROM CompY_POs939 WHERE CompY_Client939_clientID939 = 7`;
-            conn.query(qryPOnumY, (err, result4) => { //three
-                if (err) throw err;
-                poNumberY = JSON.stringify(result4);
-                JSON.parse(poNumberY).forEach(poY => { //four
-                    const checkPartY = `INSERT INTO CompY_Lines939(qty939,CompY_POs939_poNumber939,CompY_Parts939_partNo939) VALUES(?, ?, ?)`;
-                    conn.query(checkPartY, [quantity,poY.poNumber939,number.partNo939], (err, result3) => { //five
-                    if (err) throw err;
-            }) 
-        }) 
-    }) 
-}) 
-})   
+            console.log('Transaction Y Ended!');
+        });
+        conn.query("XA PREPARE " +"'"+transactionIDforY+"'", (err, result) => {
+            if (err) throw err;
+            console.log('Transaction Y Prepared!');
+        });
+
+        conn.query("XA COMMIT " +"'"+transactionIDforY+"'", (err, result) => {
+            if (err) throw err;
+            console.log('Transaction Y COMMITTED!');
+        });
+    }
+    catch (err) {
+        conn.query("XA ROLLBACK " +"'"+transactionIDforZ+"'", (err, result) => {
+            if (err) throw err;
+            console.log('Transaction Z ABORTED!');
+        });
+
+        conn.query("XA ROLLBACK " +"'"+transactionIDforX+"'", (err, result) => {
+            if (err) throw err;
+            console.log('Transaction X ABORTED!');
+        });
+
+        conn.query("XA ROLLBACK " +"'"+transactionIDforY+"'", (err, result) => {
+            if (err) throw err;
+            console.log('Transaction Y ABORTED!');
+        });
+        console.log(err);
+    }
             const qry2 = `SELECT poNumber939 FROM CompZ_POs939 WHERE CompZ_Client939_clientID939 =`+clientID;
             conn.query(qry2, (err, result1) => {
             if (err) throw err;
@@ -171,16 +210,11 @@ router.post('/submitPO', async (req, res) => {
             JSON.parse(getPO).forEach(element => {
             const qry4 = `INSERT INTO CompZ_Lines939 (CompZ_POs939_poNumber939, qty939, priceOrdered939) VALUES(?, ?, 19.99)`;
             conn.query(qry4, [element.poNumber939, quantity], (err, result4) => {
-            if (err) throw err;
-            
+            if (err) throw err;   
     }
         //end
          
         )});
-    });
-});
-});
-            
         });
         conn.release();
     });
